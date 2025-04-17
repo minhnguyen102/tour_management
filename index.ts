af203ac913ffff105826f6691882ebf5a4217938
CodeMnh <minhkhac1002@gmail.com>
2025-04-17 13:14:44 +0700

    Tạo router cơ bản + cài đặt pug

--- index.ts
+++ index.ts
@@ -5,10 +5,18 @@ dotenv.config()
 
 const port: number | string = process.env.PORT || 3000
 
+// pug
+app.set('views', './views')
+app.set('view engine', 'pug')
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Trang chủ')
-  })
+})
+
+app.get('/tours', (req: Request, res: Response) => {
+    res.render("client/pages/tours/index.pug")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
