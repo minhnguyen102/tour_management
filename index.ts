ede1b7a29cecd0b8271dc0ed2c6fc159c3e71ca4
CodeMnh <minhkhac1002@gmail.com>
2025-04-18 10:15:58 +0700

    Cài đặt static file

--- index.ts
+++ index.ts
@@ -6,7 +6,8 @@ import RouterClient from "./routes/client/index.router"
 
 const port: number | string = process.env.PORT || 3000
 
-
+// static file
+app.use(express.static('public'))
 
 // routerClient
 RouterClient(app);
@@ -14,7 +15,6 @@ RouterClient(app);
 // pug
 app.set('views', './views')
 app.set('view engine', 'pug')
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
