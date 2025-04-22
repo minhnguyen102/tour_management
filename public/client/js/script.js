7f0ac4f71ca2f840d0f9a37712280a9533602349
CodeMnh <minhkhac1002@gmail.com>
2025-04-22 15:57:30 +0700

    Add tour to cart

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -16,3 +16,33 @@ var swiper2 = new Swiper(".mySwiper2", {
     },
 });
 // End Swipper slide tour detail
+
+// Add tour to cart
+const cart = localStorage.getItem("cart");
+if(!cart){
+    localStorage.setItem("cart", JSON.stringify([]))
+}
+
+const formAddToCart = document.querySelector("[form-add-to-cart]")
+if(formAddToCart){
+    formAddToCart.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        const quantity = parseInt(e.target.elements.quantity.value);
+        const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+        
+        if(quantity > 0 && tourId){
+            const cart = JSON.parse(localStorage.getItem("cart"));
+            const indexExitTour = cart.findIndex(item => item.tourId === tourId)
+            if(indexExitTour === -1){
+                cart.push({
+                    tourId : tourId,
+                    quantity : quantity
+                })
+            }else{
+                cart[indexExitTour].quantity += quantity
+            }
+            localStorage.setItem("cart", JSON.stringify(cart))
+        }
+    })
+}
+// End Add tour to cart
